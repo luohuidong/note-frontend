ffd866fc1b58c342927159ab59ab97f3f7733958
luohuidong <luohuidong01@126.com>
2024-03-18 16:55:11 +0800

    modify build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,21 @@ echo "ROOT: $ROOT"
 echo "QUARTZ: $QUARTZ"
 
 if [ -d "quartz" ]; then
-    rm -rf quartz
+    cd $QUARTZ
+    git pull
+else
+    cd $ROOT
+    git clone https://github.com/jackyzha0/quartz.git
 fi
 
+# copy notes to quartz
 rm -rf quartz/content
 mkdir quartz/content
-git clone https://github.com/jackyzha0/quartz.git
-node ./replaceQuartzConfig.cjs
 cp -r notes/* quartz/content
+
+# modify quartz config 
+node ./replaceQuartzConfig.cjs
+
 cd quartz
 npm i 
 npx quartz build
