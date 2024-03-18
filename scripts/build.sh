e665322b28584ceb12dab0d5814ba1683ac49cee
luohuidong <luohuidong01@126.com>
2024-03-18 17:33:29 +0800

    modify build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,17 +5,9 @@ QUARTZ=$ROOT/quartz
 echo "ROOT: $ROOT"
 echo "QUARTZ: $QUARTZ"
 
-if [ -d "quartz" ]; then
-    echo "quartz exists"
-    cd $QUARTZ
-    git pull
-else
-    echo "cloning quartz"
-    cd $ROOT
-    git clone --depth 1 https://github.com/jackyzha0/quartz.git
-fi
-
-cd $ROOT
+echo "cloning quartz"
+rm -rf quartz
+git clone --depth 1 https://github.com/jackyzha0/quartz.git
 
 echo "copy notes to quartz"
 rm -rf quartz/content
